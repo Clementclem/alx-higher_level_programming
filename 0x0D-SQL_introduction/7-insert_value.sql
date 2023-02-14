5b409ecf0150982b5cbba1846ebddf114f735870
Clem <109198546+Clementclem@users.noreply.github.com>
2023-02-14 21:12:32 +0300

    Update 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
-#!/bin/bash
-db_name=$1
-mysql -u root -p $db_name -e "INSERT INTO first_table (id, name) VALUES (89, 'Best School');"
+-- inserts new row into table
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School');
